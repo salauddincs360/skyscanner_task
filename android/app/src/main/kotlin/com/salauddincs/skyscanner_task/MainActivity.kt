1e9e60521573f40ea1b26e92515be6e458ec521a
MD SALAUDDIN <salauddincs360@gmail.com>
2023-12-11 02:41:49 +0600

    Fresh Project

---
+++ android/app/src/main/kotlin/com/salauddincs/skyscanner_task/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.salauddincs.skyscanner_task
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,5 @@
+import "package:flutter/material.dart";
+import "views/home/home_screen.dart";
+void main(){
+  runApp(MaterialApp(home: HomeScreen(),));
+}
